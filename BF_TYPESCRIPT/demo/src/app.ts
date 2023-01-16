4c88776791032d048e871c0376a44548d3bd1264
Amine <aminehoussane@gmail.com>
2023-01-16 15:37:21 +0100

    custometypes TS

--- BF_TYPESCRIPT/demo/src/app.ts
+++ BF_TYPESCRIPT/demo/src/app.ts
@@ -9,7 +9,6 @@ import { mesTypes } from "./docs/types";
 console.log('mesTypes.a :>> ', mesTypes.a);
 console.log('mesTypes.b :>> ', mesTypes.b);
 console.log('mesTypes.c :>> ', mesTypes.c);
-
 console.log('mesTypes.e :>> ', mesTypes.e);
 console.log('mesTypes.f :>> ', mesTypes.f);
 console.log('mesTypes.g :>> ', mesTypes.g);
@@ -39,15 +38,15 @@ console.warn("Partie 3 - Narrowing");
 
 import { printId, exempleN, isDate, isUser, TUser } from "./docs/narrowing";
 
-printId(12)
-printId('a')
-printId(null)
+printId(12);
+printId('a');
+printId(null);
 
-exempleN('a', 'a')
-exempleN('a', true)
-exempleN(12, true)
+exempleN('a', 'a');
+exempleN('a', true);
+exempleN(12, true);
 
-let birthDate = new Date('03/04/1996')
+let birthDate = new Date('03/04/1996');
 
 console.log('Date [03/04/1996]:>> ', isDate(birthDate));
 console.log('String [03/03/1001]:>> ', isDate('03/03/1001'));
@@ -59,3 +58,34 @@ console.log('isUser(user) :>> ', isUser(user));
 /**
  * Partie 4 - Custom Types
  */
+
+console.warn("Partie 4 - Custom Types");
+
+import { Admin, Admin2, Id, DateString } from './docs/customTypes';
+
+let admin: Admin = {
+    roleId: 0,
+    lastName: "Geerts",
+    firstName: "Quentin"
+};
+
+let admin2: Admin2 = {
+    roleId: "",
+    lastName: "",
+    firstName: ""
+}
+
+console.log('admin :>> ', admin);
+
+let idInt: Id = 45;
+let idString: Id = "a";
+// let idBooleen: Id = true;
+
+console.log('idInt :>> ', idInt);
+console.log('idString :>> ', idString);
+// console.log('idBooleen :>> ', idBooleen);
+
+let date: DateString = "03/04/1996";
+let date2: DateString = new Date().toString()
+console.log('date :>> ', date);
+console.log('date :>> ', date2);
---
+++ BF_TYPESCRIPT/demo/src/docs/customTypes.ts
@@ -0,0 +1,18 @@
+/**
+ * Partie 4 - Custom Types
+ * Permet de créer ses propres types
+ */
+
+// Avantage des custom types, pas besoin de classe 
+// avec constructeur et instanciation
+
+type Admin = { roleId: number, lastName: string, firstName: string; };
+
+type Id = string | number;
+
+type Admin2 = { roleId: Id, lastName: string, firstName: string; };
+
+type DateString = string
+
+
+export { Admin, Admin2, Id, DateString }
