a5a9f0990cde4b209d2590828567d6283d10f880
Zangetsu95 <aminehoussane@gmail.com>
2023-02-02 16:57:15 +0100

    express js middleware

--- BF_WEB_API/first-step/src/index.js
+++ BF_WEB_API/first-step/src/index.js
@@ -5,6 +5,10 @@ const PORT = 3000
 
 app.use(express.json())
 // app.use(express.urlencoded)
+app.use((req, res, next) => {
+    console.log(`${req.method}:${req.url}`)
+    next()
+})
 
 
 app.listen(PORT, () => console.log(`runing express at port ${PORT} `))
@@ -20,6 +24,19 @@ const groceryList = [
     }]
 
 //--------------------------------
+// app.get('/groceries', (req, res, next) => {
+//     console.log("before handling request")
+//     next()
+// }, (req, res, next) => {
+//     res.send(groceryList)
+//     next()
+// },
+//     (req, res, next) => {
+//         console.log("Finished executing GET request")
+//         next()
+//     },
+// )
+
 app.get('/groceries', (req, res) => {
     res.send(groceryList)
 })
@@ -30,3 +47,5 @@ app.post('/groceries', (req, res) => {
     groceryList.push(req.body)
     res.send(201)
 })
+
+
