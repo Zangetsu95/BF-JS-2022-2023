fce08d4a68054d62c2d99115f24a4d5d69d47c0e
Zangetsu95 <aminehoussane@gmail.com>
2023-03-29 14:15:12 +0200

    testing validating request data with pipes

--- BF_WEB_API/NEST/messages/src/messages/messages.controller.ts
+++ BF_WEB_API/NEST/messages/src/messages/messages.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { get } from 'http';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -7,8 +7,12 @@ export class MessagesController {
   listMessages() {}
 
   @Post()
-  createMessages() {}
+  createMessages(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
 
   @Get('/:id')
-  getMessages() {}
+  getMessages(@Param('id') id: string) {
+    console.log(id);
+  }
 }
