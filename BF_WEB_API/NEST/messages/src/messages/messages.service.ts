1ea760e262a0585e3db70506c1bff1277e0c7897
Zangetsu95 <aminehoussane@gmail.com>
2023-03-30 17:49:07 +0200

    adding file in folder messages

---
+++ BF_WEB_API/NEST/messages/src/messages/messages.service.ts
@@ -0,0 +1,23 @@
+import { MessageRepository } from './messages.repository';
+
+export class MessagesService {
+  messagesRepo: MessageRepository;
+
+  constructor() {
+    //services is creating its own dependencies
+    //DONT DO THIS ON REAL APPS
+    this.messagesRepo = new MessageRepository();
+  }
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
