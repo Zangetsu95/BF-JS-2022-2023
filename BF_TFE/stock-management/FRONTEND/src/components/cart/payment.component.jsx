75975009ac3f1397921e3cb583d168aefe23922f
Zangetsu95 <aminehoussane@gmail.com>
2023-04-25 20:23:15 +0200

    TFE : fixing stripe issue paymen success

--- BF_TFE/stock-management/FRONTEND/src/components/cart/payment.component.jsx
+++ BF_TFE/stock-management/FRONTEND/src/components/cart/payment.component.jsx
@@ -1,11 +1,25 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
 import axios from "axios"
 import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { clearCart } from "../../store/actions/cartActions"
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
+} from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
 const PaymentForm = ({ onSuccess, cartItems, userId }) => {
   const [error, setError] = useState(null)
   const stripe = useStripe()
   const elements = useElements()
+  const [isAlertOpen, setIsAlertOpen] = useState(false)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -21,42 +35,37 @@ const PaymentForm = ({ onSuccess, cartItems, userId }) => {
     }
 
     try {
-      const response = await axios.post("http://127.0.0.1:5000/api/stripe", {
-        paymentMethodId: paymentMethod.id,
-      })
-      console.log(response)
-      if (response.data.success) {
-        try {
-          const transactionResponse = await axios.post(
-            "http://127.0.0.1:5000/api/transaction",
-            {
-              type: "sale",
-              product_id: cartItems.id,
-              quantity: cartItems.quantity,
-              price: cartItems.price,
-              user_id: userId,
-              date: new Date().toISOString(),
-            }
-          )
-          console.log("transactionResponse :>> ", transactionResponse)
-
-          if (transactionResponse.status === 200) {
-            onSuccess()
-          } else {
-            setError("Erreur lors de l'enregistrement de la transaction.")
-          }
-          console.log("transactionResponse :>> ", transactionResponse)
-        } catch (error) {
-          console.log(error)
-          setError("Erreur lors de l'enregistrement de la transaction.")
+      // Create a transaction for each item in the cart
+      for (let item of cartItems) {
+        const transactionData = {
+          type: "sale",
+          product_id: item.id,
+          quantity: item.quantity,
+          price: item.price,
+          user_id: userId,
+          date: new Date().toISOString(),
         }
-      } else {
-        setError("Erreur lors du paiement avant le catch.")
+        const transactionResponse = await axios.post(
+          "http://127.0.0.1:5000/api/transaction",
+          transactionData
+        )
+        if (transactionResponse.status !== 201) {
+          setError("Erreur lors de l'enregistrement de la transaction.")
+          return
+        }
       }
+      dispatch(clearCart())
+      setIsAlertOpen(true)
+      onSuccess()
     } catch (error) {
       console.log(error)
-      setError("Erreur lors du paiement.")
+      setError("Erreur lors de l'enregistrement des transactions.")
     }
+  }
+
+  const handleAlertClose = () => {
+    setIsAlertOpen(false)
+    navigate("/")
   }
 
   const cardElementOptions = {
@@ -82,13 +91,33 @@ const PaymentForm = ({ onSuccess, cartItems, userId }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement options={cardElementOptions} />
-      <button type="submit" disabled={!stripe}>
-        Procéder au paiement
-      </button>
-      {error && <div>{error}</div>}
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement options={cardElementOptions} />
+        <button type="submit" disabled={!stripe}>
+          Procéder au paiement
+        </button>
+        {error && <div>{error}</div>}
+        <Dialog
+          open={isAlertOpen}
+          onClose={handleAlertClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Paiement effectué !</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Le paiement s'est bien effectué !.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleAlertClose} color="primary" autoFocus>
+              Ok
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </form>
+    </>
   )
 }
 
